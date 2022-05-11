f2c8ae15a924a9d330a9e7243dbe0bf5d900040a
elfsvet <stepanmatysik@gmail.com>
2022-05-11 13:27:07 -0400

    added potion mock in player contribution

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,7 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+console.log(new Potion());
 // require a module. we don't need to provide the js extension
 const Player = require('../lib/Player');
 // write a test that will fail
@@ -8,5 +12,10 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+
 });
 // write minimal functional code in Player.js to make it pass
